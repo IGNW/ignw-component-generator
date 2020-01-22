185c1d63d4e7abc94d2263657d78c32bc0a7c0a2
Desmond Jones <djones@ignw.io>
2020-01-22 13:59:38 -0800

    remove jest mock reset from test template

--- templates/test.tsx
+++ templates/test.tsx
@@ -4,8 +4,6 @@ import { render } from '@testing-library/react'
 import {{pascalCase name}} from '.'
 
 describe('{{pascalCase name}}', () => {
-  beforeEach(() => jest.clearAllMocks())
-
   it('should render the {{pascalCase name}} component', () => {
     const props = {}
     const component = render(<{{pascalCase name}} {...props} />)
