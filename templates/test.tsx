e25c367860a9270ccdb8e561894519f75af50866
Desmond Jones <djones@ignw.io>
2020-01-22 10:49:38 -0800

    use handlebard replacers

--- templates/test.tsx
+++ templates/test.tsx
@@ -3,10 +3,10 @@ import { render } from '@testing-library/react'
 
 import {{pascalCase name}} from '.'
 
-describe('<%= componentName %>', () => {
+describe('{{pascalCase name}}', () => {
   beforeEach(() => jest.clearAllMocks())
 
-  it('should render the <%= componentName %> component', () => {
+  it('should render the {{pascalCase name}} component', () => {
     const props = {}
     const component = render(<{{pascalCase name}} {...props} />)
     expect(component).toBeTruthy()
